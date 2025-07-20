5fa0d553290ce3d59f4d3c23bda29d0aa374118d
Mirco <mirco@MacBook-Pro-von-Mirco.local>
2025-07-20 15:23:24 +0200

    Add Balance component

---
+++ src/components/Balance.vue
@@ -0,0 +1,15 @@
+<template>
+  <h4>Your Balance</h4>
+  <h1 id="balance">${{ balance }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
